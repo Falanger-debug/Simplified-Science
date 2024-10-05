3df9683c70a7ae45ed276096e5500e52e00963ff
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2024-10-05 16:55:17 +0200

    a

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/streszczenie')
 def about():
-    return "This is the about page."
+    return render_template('streszczenie.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
