fbceee0021011706e1d5d203d5fd3373aa5b3396
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2024-10-05 20:40:07 +0200

    test custom tytułu

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def home():
     if request.method == 'POST':
         link = request.form.get('link')  # Użyj get() dla bezpieczeństwa
         knowledge = request.form.get('poziom-wiedzy')
-
+        title= "Sztywny tytuł artykułu"
         # Zwracamy szablon streszczenie.html, przekazując zmienne
-        return render_template('streszczenie.html', link=link, knowledge=knowledge)
+        return render_template('streszczenie.html', link=link, knowledge=knowledge, title=title)
 
     # Przy metodzie GET wyświetlaj index.html
     return render_template('index.html')
