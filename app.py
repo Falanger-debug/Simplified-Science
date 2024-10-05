a0f39c1656921ecc0cb7d9b3b00fbd64960aa793
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2024-10-05 17:46:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     link = None  # Inicjalizujemy link jako None
     if request.method == 'POST':
         link = request.form['link']  # Odbieramy wartość z formularza
-        return render_template('streszczenie.html')  # Przekazujemy link do szablonu  
+        return "CHUJ"
 
     return render_template('index.html')  # Przekazujemy link do szablonu  
 
