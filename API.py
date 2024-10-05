5cba8240f904a332a269db258eef646b2ec85589
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2024-10-05 21:09:41 +0200

    Update API.py

--- API.py
+++ API.py
@@ -11,27 +11,27 @@ genai.configure(api_key="AIzaSyArNS7cIvztvHVwPjGgpXJVpbwj_8dJ6MY")
 
 # Create the model
 generation_config = {
-  "temperature": 0.7,
-  "top_p": 0.95,
-  "top_k": 40,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
+"temperature": 0.7,
+"top_p": 0.95,
+"top_k": 40,
+"max_output_tokens": 8192,
+"response_mime_type": "text/plain",
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-pro-002",
-  generation_config=generation_config,
-  # safety_settings = Adjust safety settings
-  # See https://ai.google.dev/gemini-api/docs/safety-settings
+model_name="gemini-1.5-pro-002",
+generation_config=generation_config,
+# safety_settings = Adjust safety settings
+# See https://ai.google.dev/gemini-api/docs/safety-settings
 )
 
 chat_session = model.start_chat(
-  history=[
-  ]
+history=[
+]
 )
 
 def getTitle(link):
-  prompt = "Tell me what the title of the article is. Just the title based on this AND ONLY THIS! link. I need just the title. Don't write 'The title of this article is THE_TITLE', just 'THE_TITLE'. Be honest."
+    prompt = "Tell me what the title of the article is. Just the title based on this AND ONLY THIS! link. I need just the title. Don't write 'The title of this article is THE_TITLE', just 'THE_TITLE'. Be honest."
 
-  response = chat_session.send_message(prompt + link)
-  return response.text
+    response = chat_session.send_message(prompt + link)
+    return response.text
